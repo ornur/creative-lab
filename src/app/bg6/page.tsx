bc96813aceb9676d60335f7dab2ec67e77eb9fec
Nurda <nurdauletaidaruli@gmail.com>
2026-03-30 20:31:37 +0500

    refactor: remove Plasma, SoftAurora, Threads, and DropdownMenu components
    - Deleted Plasma component which handled OGL rendering for plasma effects.
    - Removed SoftAurora component responsible for rendering soft aurora effects with shaders.
    - Eliminated Threads component that created visual threads using WebGL.
    - Removed DropdownMenu component and its related subcomponents for dropdown functionality.

--- src/app/bg6/page.tsx
+++
@@ -1,9 +0,0 @@
-import Threads from "@/components/Threads";
-
-export default function BG5() {
-  return (
-    <div className="relative h-screen w-full dark:bg-black">
-      <Threads amplitude={1.5} distance={0.1} enableMouseInteraction={false} />
-    </div>
-  );
-}
